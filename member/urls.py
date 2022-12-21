06924cb81c84a8dd5cd8ddce746cf3a98866b43e
Cushione <maulerovak@gmail.com>
2022-12-21 10:26:05 +0000

    Implement login

--- member/urls.py
+++ member/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'member'
 
 urlpatterns = [
+    path("login", views.login_request, name="login"),
     path("register", views.register_request, name="register"),
     path("logout", views.logout_request, name="logout")
 ]
--- member/views.py
+++ member/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
+
+def login_request(request):
+    form = AuthenticationForm()
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("news:home")
+        messages.error(request, "Invalid username or password.")
+    return render(request, template_name="login.html", context={"login_form":form})
 
 def register_request(request):
     form = RegisterForm()
