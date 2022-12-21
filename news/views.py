810821c8ee619a355862d91c817a78945fd7384d
Cushione <maulerovak@gmail.com>
2022-12-21 15:32:22 +0000

    Add latest news article to home page

--- news/views.py
+++ news/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Article
 
-def Home(request): 
-	return render(request, template_name="home.html")
+def Home(request):
+	queryset = Article.objects.filter(status=1)
+	latest_article = queryset.latest('published_on')
+	main_article_list = queryset.exclude(id=latest_article.id)
+	return render(
+		request, 
+		template_name="home.html", 
+		context={
+			'latest_article': latest_article,
+			'main_article_list': main_article_list
+			}
+		)
